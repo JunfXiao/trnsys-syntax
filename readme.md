c49cf9d539f35fb825ba3582030bd9979a7bb05b
JunfXiao <junfeng.xiao@outlook.com>
2025-06-07 14:20:55 +0200

    General Improvement

--- readme.md
+++ readme.md
@@ -4,6 +4,12 @@ AST (Abstract Syntax Tree) for TRNSYS input files.
 
 Currently, it only supports the TRNSYS 18 syntax.
 
+## What is this?
+This is a package that provides an AST for TRNSYS input files, allowing you to parse, 
+manipulate, and analyze TRNSYS input files programmatically. 
+It is designed to be used as rust or python package and can be extended to 
+support additional features such as validation, output generation, and more.
+
 # Roadmap
 - [x] TRNSYS 18 Read Support
 - [x] Tests
--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,41 +1,37 @@
-
-mod equation_error;
 mod content_error;
+mod equation_error;
 mod report;
 
-use strum_macros::Display;
-pub use equation_error::*;
 pub use content_error::*;
+pub use equation_error::*;
+pub use error_stack::Result as ErrorStackResult;
+pub use nom::Err as NomErr;
 pub use report::*;
 pub use std::error::Error as StdError;
 pub use std::io::Error as IoError;
-pub use nom::Err as NomErr;
-pub use error_stack::Result as ErrorStackResult;
+use strum_macros::Display;
 
 // src/error.rs
-use thiserror::Error;
-use std::num::{ParseFloatError, ParseIntError};
 use nom::error::ErrorKind;
-use std::fmt::Debug;
-use std::convert::Infallible;
 use std::borrow::Cow;
+use std::convert::Infallible;
+use std::fmt::Debug;
+use std::num::{ParseFloatError, ParseIntError};
 use std::str::ParseBoolError;
+use thiserror::Error;
 
 /// Error that occurred during parsing
 // #[derive(Debug, Error)]
 // pub enum Error {
 //     #[error(transparent)]
 //     Syntax(#[from] SyntaxError),
-//     
+//
 //     #[error("IO Error: {0}")]
 //     Io(#[from] std::io::Error),
 // }
 
-
-
-
 #[derive(Debug, Clone, Copy, Display)]
-pub enum ErrorScope{
+pub enum ErrorScope {
     Document,
     Block,
     Row,
@@ -44,35 +40,24 @@ pub enum ErrorScope{
     Expression,
 }
 
-pub type ParseResult<T, E=Error> = ErrorStackResult<T, E>;
-
-
+pub type ParseResult<T, E = Error> = ErrorStackResult<T, E>;
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("Unknown keyword is used: \"{keyword}\"")]
-    UnknownKeyword{
-        keyword: String,
-        scope: ErrorScope,
-    },
-    
+    UnknownKeyword { keyword: String, scope: ErrorScope },
+
     #[error("Unexpected {scope}: {message}")]
-    UnexpectedContent{
-        message: String,
-        scope: ErrorScope,
-    },
-    
-    #[error("Cannot Convert \"{input:.20}\" to type \"{target}\"")]
-    ConversionError{
-        input: String,
-        target: String,
-    },
-    
+    UnexpectedContent { message: String, scope: ErrorScope },
+
+    #[error("Cannot Convert \"{}\" to type \"{target}\"",self.text_short())]
+    ConversionError { input: String, target: String },
+
     #[error(transparent)]
     ContentError(#[from] ContentError),
-    
-    #[error("Invalid {scope}: '{kind:?}' Failed\n \"{input:.20}\"...")]
-    GeneralError{
+
+    #[error("Invalid {scope}: '{kind:?}' Failed in \"{}\"", self.text_short())]
+    GeneralError {
         input: String,
         kind: ErrorKind,
         scope: ErrorScope,
@@ -80,39 +65,43 @@ pub enum Error {
 
     #[error("IO Error: {0}")]
     Io(#[from] std::io::Error),
-    
 }
 
 impl Error {
     pub fn input(&self) -> Cow<str> {
         match self {
-            Error::UnknownKeyword { keyword,.. } => keyword.into(),
+            Error::UnknownKeyword { keyword, .. } => keyword.into(),
             Error::ConversionError { input, .. } => input.into(),
-            Error::GeneralError { input,  .. } => input.into(),
+            Error::GeneralError { input, .. } => input.into(),
             Error::ContentError(c) => c.text(),
             _ => "-".into(),
         }
+    }
+
+    pub fn text_short(&self) -> Cow<str> {
+        let text = self.input().trim().to_string();
+        text.split_once('\n')
+            .map(|(before, _)| before.trim().to_string())
+            .unwrap_or(text)
+            .to_string()
+            .into()
     }
 }
 
 pub type RError = ReportWrapper<Error>;
 
-
 impl<'a, T> NewFromErrorKind<T> for Error
-where T: ToString
+where
+    T: ToString,
 {
     fn new_from_error_kind(input: T, kind: ErrorKind, scope: ErrorScope) -> Self {
-        
         Error::GeneralError {
             input: input.to_string(),
             kind,
-            scope
+            scope,
         }
     }
 }
-
-
-
 
 impl From<strum::ParseError> for Error {
     fn from(e: strum::ParseError) -> Self {
@@ -153,7 +142,6 @@ impl From<ParseFloatError> for Error {
 
 impl From<ParseBoolError> for Error {
     fn from(value: ParseBoolError) -> Self {
-        
         Error::UnexpectedContent {
             message: format!("Failed to parse boolean: {:?}", value),
             scope: ErrorScope::Param,
@@ -169,7 +157,3 @@ impl From<Infallible> for Error {
         }
     }
 }
-
-
-
-
